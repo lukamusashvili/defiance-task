6c199f8060c5de68a5e23415e0922d5e737dbda4
lukamusashvili <lukamusashvili@gmail.com>
2026-05-17 19:45:19 +0400

    Task 1 completed: Fix auth endpoints to return 401 on failure

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('jwt-path');
-require('dotenv').config();
-const userRouter = require('./routes/userRoute.js');
-const foodRouter = require('./routes/foodRoute.js');
-const cartRouter = require('./routes/cartRoute.js');
-const orderRouter = require('./routes/orderRoute.js');
-const { connectDB } = require('./config/db.js');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const userRouter = require("./routes/userRoute.js");
+const foodRouter = require("./routes/foodRoute.js");
+const cartRouter = require("./routes/cartRoute.js");
+const orderRouter = require("./routes/orderRoute.js");
+const { connectDB } = require("./config/db.js");
 
 // app config
 const app = express();
@@ -15,16 +14,15 @@ const port = 4000;
 // middleware
 app.use(express.json());
 app.use(cors());
-app.use(dotenv());
 // db connection
 connectDB();
 
 // api endpoints
 app.use("/api/food", foodRouter);
-app.use("/images", express.static('uploads'));
-app.use('/api/user', userRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
     res.send("API working");
